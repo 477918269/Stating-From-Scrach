62ac03ef01c93267e4c386fb54c19d3e10d0756c
用户名 <>
2019-10-16 11:38:22 +0800

    知识体系已经搭建完毕，现在要做的是提高代码能力，目的是打造真正过硬的代码能力

---
+++ 第一天/第一天/源.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+
+using namespace std;
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		:_year(year),
+//		_month(month),
+//		_day(day)
+//	{
+//	}
+//
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//
+//	Date& operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_year = d._year;
+//		}
+//		return *this;
+//	}
+//
+//	~Date()
+//	{
+//		_year = _month = _day = 0;
+//	}
+//
+//	void play() const
+//	{
+//		cout << _year << endl;
+//	}
+//
+//	void change(Date b)
+//	{
+//		b._year = 1000;
+//		play();
+//	}
+//
+//	Date* operator&()
+//	{
+//		return d;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//int main()
+//{
+//	int a = 0;
+//	int &b = a;
+//	cout << b << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//class Date
+//{
+//public:
+//	Date()
+//	{}
+//
+//	Date(int year, int month, int day)
+//		:_year(year),
+//		 _month(month),
+//		 _day(day)
+//	{
+//	}
+//
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//
+//	}
+//
+//	Date& operator= (Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//	}
+//
+//	~Date()
+//	{}
+//
+//	Date* operator&()
+//	{
+//		return this;
+//	}
+//
+//
+//public:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//#include<string>
+//class Date
+//{
+//public:
+//	
+//private:
+//	string s = "hellow world";
+//};
+//
+//int main()
+//{
+//	
+//	system("pause");
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date()
+//	{}
+//
+//	Date(const Date& d)
+//	{
+//
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _year;
+//};
+//
+//int main()
+//{
+//	return 0;
+//}
